7ad9db722fb0ae3da7e2fd46699da00476d19859
stancillous <stancillousray@gmail.com>
2025-02-03 17:42:44 +0300

    include main.css

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import App from '../App.vue'
+import '../assets/main.css'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,16 +8,14 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: App,
+      component: () => import('../views/Index.vue'),
+      // component: App,
     },
-    // {
-    //   path: '/about',
-    //   name: 'about',
-    //   // route level code-splitting
-    //   // this generates a separate chunk (About.[hash].js) for this route
-    //   // which is lazy-loaded when the route is visited.
-    //   component: () => import('../views/AboutView.vue'),
-    // },
+    {
+      path: '/get-quote',
+      name: 'get-quote',
+      component: () => import('../views/GetQuote.vue'),
+    },
   ],
 })
 
