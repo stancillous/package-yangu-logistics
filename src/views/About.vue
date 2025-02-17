c135252c49784388143860f930a0e78c4b7cd706
stancillous <stancillousray@gmail.com>
2025-02-17 11:38:36 +0300

    about us page

---
+++ src/views/About.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <div class="" style="max-width: 10600px; margin: auto;">
+            <img src="https://cdn.prod.website-files.com/65636268c62f3d0464956d0b/656780079843f7bfe6cf4574_pexels-tom-fisk-3057960%201.jpg" alt="" width="100%" height="350" style="object-fit: cover;">
+        </div>
+        <div class="container">
+            <div class="row py-5 my-5">
+                <div class="col-md-6">
+                    <small class="text-muted text-uppercase d-inline-block mb-2">who we are</small>
+                    <h2 class="fw-bold display-6 mb-4 text-muted">About Packageyangu Logistics</h2>
+                </div>
+                <div class="col-md-6">
+                    <h4 class="about-txt">"Imagine a world where every package arrives not just on time, but with care, precision, and a personal touch. At Package Yangu Logistics, we don’t just deliver packages—we deliver peace of mind. Whether it’s a cherished gift, a critical business document, or a piece of your dreams, we treat it like it’s our own. Fast, reliable, and always with a smile. Don’t just ship—experience the difference. Choose Package Yangu Logistics today, and let’s move what matters to you!"</h4>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+.about-txt{
+    line-height: 160%;
+    font-size: 20px;
+}
+
+@media screen and (max-width: 900px) {
+    .about-txt{
+        font-size: 18px;
+    }
+}
+</style>
