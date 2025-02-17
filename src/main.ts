3034086911f277a9b483758681662300bdb1734a
stancillous <stancillousray@gmail.com>
2025-02-17 11:37:41 +0300

    use vue toast for page notifications

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import ToastPlugin from 'vue-toast-notification';
 
 // import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue'
 
@@ -13,6 +14,12 @@ const app = createApp(App)
 // app.use(BootstrapVue)
 // app.use(BootstrapVueIcons)
 
+app.use(ToastPlugin);
+app.use(ToastPlugin, {
+    position: 'top-right', // Set default position
+    duration: 5000, // Optional: Set default duration
+    dismissible: true // Optional: Allow dismissing the toast
+  });
 app.use(router)
 
 app.mount('#app')
