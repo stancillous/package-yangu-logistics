42167f0863a775db7369788146a84dcae2600639
Peter <mainapeter673@gmail.com>
2025-02-19 07:49:32 +0300

    rollback pics

--- src/views/GetQuote.vue
+++ src/views/GetQuote.vue
@@ -2,7 +2,7 @@
   <div class="py-1">
 
     <div class="" style="max-width: 10600px; margin: auto;">
-      <img src="/public/images/WhatsApp Image 2025-02-18 at 21.48.49 (2).jpeg" alt="" width="100%" height="350" style="object-fit: cover;">
+      <img src="/WhatsApp Image 2025-02-18 at 21.48.49 (2).jpeg" alt="" width="100%" height="350" style="object-fit: cover;">
     </div>
 
     <GetQuote/>
